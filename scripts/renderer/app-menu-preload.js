39b82f934dfc6cc0a27c1221e06c06fdd3c8c97b
Pawel Psztyc <jarrodek@gmail.com>
2018-12-14 12:18:47 -0800

    Fixing menu loading

--- scripts/renderer/app-menu-preload.js
+++ scripts/renderer/app-menu-preload.js
@@ -1,5 +1,5 @@
 const {ipcRenderer: ipc} = require('electron');
-const {ThemeManager} = require('../packages/sources-manager/renderer');
+const {ThemeManager} = require('../packages/themes-manager/renderer');
 const {ArcPreferencesProxy} = require('../packages/arc-preferences/renderer');
 process.once('loaded', () => {
   global.ipc = ipc;
