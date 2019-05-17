27518b78a6a91d46d4dcb669811ced08fee7a2c2
Pawel <jarrodek@gmail.com>
2019-05-16 19:43:20 -0700

    New: Adding a function to test for embedded node_modules

--- tasks/prepare-app.js
+++ tasks/prepare-app.js
@@ -1,11 +1,12 @@
 const fs = require('fs-extra');
+const path = require('path');
 /**
  * A logic to be executed when the app is being installed from sources (local npm install).
  */
 class PrepareApp {
   run() {
     return this.fixMarked()
-
+    .then(() => this.analyzeNodeModules())
     .catch((cause) => {
       console.error(cause);
       process.exit(-1);
@@ -22,6 +23,38 @@ class PrepareApp {
       content = content.replace('new marked.Renderer()', 'new marked.default.Renderer()');
       content = content.replace('marked(this.markdown', 'marked.default(this.markdown');
       return fs.writeFile(file, content);
+    });
+  }
+  /**
+   * A function that checks whether any of `@advanced-rest-client` or `@api-components`
+   * module located in node_modules has inner `node_modules` component.
+   * If it has then this function resolves to error (none of the component should have
+   * recursive modules dir).
+   *
+   * @return {Promise}
+   */
+  analyzeNodeModules() {
+    return this._analyzeNmPath('@advanced-rest-client')
+    .then(() => this._analyzeNmPath('@api-components'));
+  }
+
+  _analyzeNmPath(dir) {
+    const loc = path.join(__dirname, '..', 'node_modules', dir);
+    return fs.readdir(loc)
+    .then((items) => {
+      for (let i = 0, len = items.length; i < len; i++) {
+        const candidate = path.join(loc, items[i], 'node_modules');
+        let has = false;
+        try {
+          const stat = fs.statSync(candidate);
+          if (stat.isDirectory()) {
+            has = true;
+          }
+        } catch (e) {}
+        if (has) {
+          throw new Error(`Nested node_modules directory exists for ${candidate}`);
+        }
+      }
     });
   }
 }
