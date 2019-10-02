1195d121fa2b0116475880ac96fc2b5e4d0b15b2
Pawel Psztyc <jarrodek@gmail.com>
2019-10-02 08:24:31 -0700

    chore: reconfiguring build process

---
+++ tasks/arc-publish.sh
@@ -0,0 +1,14 @@
+if [ $TRAVIS_BRANCH != 'develop' ]; then
+  exit 0
+fi
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  docker run --rm \
+    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
+    -v ${PWD}:/project \
+    -v ~/.cache/electron:/root/.cache/electron \
+    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+    electronuserland/builder:wine \
+    /bin/bash -c "./node_modules/.bin/electron-builder --linux --win"
+else
+  /bin/bash -c "./node_modules/.bin/electron-builder --mac"
+fi
