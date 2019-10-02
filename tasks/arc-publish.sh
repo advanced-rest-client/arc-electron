bee5906717d2d19a0185f990c2bc0f5ba3142789
Pawel <jarrodek@gmail.com>
2019-10-02 10:24:08 -0700

    build: adding builder variables to the app build file

--- tasks/arc-publish.sh
+++ tasks/arc-publish.sh
@@ -2,6 +2,11 @@ if [ $TRAVIS_BRANCH != 'develop' ] && [ $TRAVIS_BRANCH != 'master' ] && [ $TRAVI
   echo "Skipping ARC build. Incompatible branch."
   exit 0
 fi
+
+export CSC_NAME="Pawel Psztyc"
+export WIN_CSC_LINK="$(pwd)/tasks/advancedrestclient.pfx"
+export CSC_LINK="$(pwd)/tasks/mac-app-distribution-cert.p12"
+
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   docker run --rm \
     --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
