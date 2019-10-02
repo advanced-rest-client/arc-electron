ae98f45dcf2411777b0eefed2b17a51148ca7bb8
Pawel <jarrodek@gmail.com>
2019-10-02 11:57:08 -0700

    build: moving keys decryption into rthe publish script

--- tasks/arc-publish.sh
+++ tasks/arc-publish.sh
@@ -7,6 +7,9 @@ export CSC_NAME="Pawel Psztyc"
 export WIN_CSC_LINK="$(pwd)/tasks/advancedrestclient.pfx"
 export CSC_LINK="$(pwd)/tasks/mac-app-distribution-cert.p12"
 
+openssl aes-256-cbc -K $encrypted_36ee0d5a95ce_key -iv $encrypted_36ee0d5a95ce_iv -in tasks/advancedrestclient.pfx.enc -out $WIN_CSC_LINK -d
+openssl aes-256-cbc -K $encrypted_36ee0d5a95ce_key -iv $encrypted_36ee0d5a95ce_iv -in tasks/mac-app-distribution-cert.p12.enc -out $CSC_LINK -d
+
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   docker run --rm \
     --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
