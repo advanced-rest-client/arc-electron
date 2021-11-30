5e5024e27370d34e95f1d3fe77cb29eb779de812
Pawel Psztyc <jarrodek@gmail.com>
2021-11-30 15:42:59 -0800

    fix: fixes #423 - enabling authorization processor
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- tasks/index.js
+++ tasks/index.js
@@ -3,6 +3,7 @@
 import '@advanced-rest-client/app/define/api-entrypoint-selector.js';
 import '@advanced-rest-client/base/define/variables-suggestions.js';
 
+export { RequestAuthorization, ResponseAuthorization, ModulesRegistry } from '@advanced-rest-client/base'
 export * from '@advanced-rest-client/app';
 export * from '@advanced-rest-client/events';
 export * as IdbKeyVal from 'idb-keyval';
